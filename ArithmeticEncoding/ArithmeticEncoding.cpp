e3732277c1a66ee2d5b6f36d0e67bd129c701bc1
daywish <volodymyr.korobko@nure.ua>
2022-12-16 21:20:54 +0200

    Добавьте файлы проекта.

---
+++ ArithmeticEncoding/ArithmeticEncoding.cpp
@@ -0,0 +1,199 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <utility>
+#include <map>
+#include <vector>
+#include <tuple>
+
+std::string string_to_compress = "КОВ.КОРОВА";
+std::map<char, std::pair<double, double>> intervals = {
+    {'О', {0, 0.3}}, {'К', {0.3, 0.5}}, {'В', {0.5, 0.7}}, 
+    {'Р', {0.7, 0.8}}, {'А', {0.8, 0.9}}, {'.', {0.9, 1}}
+};
+
+float randomDouble(double min, double max)
+{
+    double res = (double)rand() / (double)RAND_MAX;
+    return min + res * (max - min);
+}
+
+double encodeByArithmeticEncoding(std::string input) {
+    double left = 0;
+    double right = 1;
+    double previous_left = 0;
+    double previous_range = 0;
+
+    for (const auto& symbol : input) {
+        previous_left = left;
+        previous_range = right - left;
+        left = previous_left + intervals[symbol].first * previous_range;
+        right = previous_left + intervals[symbol].second * previous_range;
+    }
+
+    return randomDouble(left, right);
+}
+
+std::string decodeByArithmeticEncoding(
+    double encoded_value, size_t input_length
+) {
+    std::string decoded_value = "";
+    double left = 0;
+    double right = 1;
+    double symbol_left = 0;
+    double symbol_right = 0;
+    double previous_left = 0;
+    double previous_range = 0;
+
+    for (size_t i = 0; i < input_length; ++i) {
+        for (const auto& interval : intervals) {
+            previous_left = left;
+            previous_range = right - left;
+            symbol_left = previous_left + interval.second.first * previous_range;
+            symbol_right = previous_left + interval.second.second * previous_range;
+
+            if (symbol_left <= encoded_value && symbol_right > encoded_value) {
+                decoded_value += interval.first;
+                left = symbol_left;
+                right = symbol_right;
+                break;
+            }
+        }
+    }
+
+    return decoded_value;
+}
+
+void bitsPlusFollow(
+    std::string& encoded_string, int bit, 
+    int bits_to_follow
+) {
+    encoded_string += bit;
+    for (; bits_to_follow > 0; bits_to_follow--) {
+        encoded_string += bit;
+    }
+}
+
+size_t getSymbolIndex(
+    std::vector<std::pair<char, unsigned char>> table,
+    char symbol
+) {
+    for (int i = 0; i < table.size(); ++i) {
+        if (table[i].first == symbol) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void advancedEncodeByArithmeticEncoding(
+    std::string input, 
+    std::vector<std::pair<char, unsigned char>> table
+) {
+    std::string result = "";
+    int left = 0;
+    int right = 65535;
+    int delitel = table[table.size() - 1].second;
+    int first_qtr = (right + 1) / 4;
+    int half = first_qtr * 2;
+    int third_qtr = first_qtr * 3;
+    int bits_to_follow = 0; 
+    int previous_left = 0;
+    int previous_range = 0;
+
+    for (const auto& symbol : input) {
+        size_t j = getSymbolIndex(table, symbol);
+        previous_left = left;
+        previous_range = right - previous_left + 1;
+
+        left = previous_left + 
+            table[j - 1].second * previous_range / delitel;
+        right = previous_left + 
+            table[j - 1].second * previous_range / delitel - 1;
+        for (;;) {
+            if (right < half) {
+                bitsPlusFollow(result, bits_to_follow, 0);
+            }
+            else if (left >= half) {
+                bitsPlusFollow(result, bits_to_follow, 1);
+                left -= half;
+                right -= half;
+            } else if ((left >= first_qtr) && (right < third_qtr)) {
+                ++bits_to_follow;
+                left -= first_qtr;
+                right -= first_qtr;
+            } else {
+                break;
+            }
+            left += left;
+            right += right + 1;
+        }
+    }
+}
+
+std::string advancedDecodeByArithmeticEncoding(
+    std::string encodedData,
+    std::vector<std::pair<char, unsigned char>> table
+) {
+    std::string result = "";
+    int left = 0;
+    int right = 65535;
+    int delitel = table[table.size() - 1].second;
+    int first_qtr = (right + 1) / 4;
+    int half = first_qtr * 2;
+    int third_qtr = first_qtr * 3;
+    int previous_left = 0;
+    int previous_range = 0;
+    int freq = 0;
+
+    for (const auto& symbol : encodedData) {
+        char value = symbol;
+        freq = ((symbol - left + 1) * delitel - 1) / (right - left + 1);
+        size_t j = getSymbolIndex(table, symbol);
+        previous_left = left;
+        previous_range = right - previous_left + 1;
+
+        left = previous_left +
+            table[j - 1].second * previous_range / delitel;
+        right = previous_left +
+            table[j - 1].second * previous_range / delitel - 1;
+        for (;;) {
+            if (right < half)
+                ;
+            else if (left >= half) {
+                left -= half;
+                right -= half;
+                value -= half;
+            }
+            else if ((left >= first_qtr) && (right < third_qtr)) {
+                value -= first_qtr;
+                left -= first_qtr;
+                right -= first_qtr;
+            }
+            else {
+                break;
+            }
+            left += left;
+            right += right + 1;
+            value += value;
+        }
+        result += value;
+    }
+
+    return result;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Ukrainian");
+    double encoded_data = encodeByArithmeticEncoding(string_to_compress);
+    std::cout << "Вхідне значення: " << string_to_compress << std::endl;
+    std::cout << "Закодоване значення: " << encoded_data << std::endl;
+    std::cout << "Декодоване значення: " << decodeByArithmeticEncoding(
+        encoded_data, 
+        string_to_compress.length()
+    );
+}
+
---
+++ MultimediaTask2/MultimediaTask2.cpp
@@ -0,0 +1,347 @@
+﻿#include <iostream>
+#include <utility>
+#include <map>
+#include <vector>
+#include <bitset>
+#include <sstream>
+#include <Windows.h>
+
+template <class T>
+class Node
+{
+public:
+    Node(T symbol) : symbol(symbol), last_code_symbol(-1) {};
+
+    Node(Node<T>* first_child, Node<T>* second_child) 
+        : last_code_symbol(-1), first_child(first_child),
+        second_child(second_child) {};
+
+    ~Node() {
+        delete first_child;
+        delete second_child;
+    }
+
+    std::string getCodeStringRepresentation() {
+        std::stringstream result;
+        for (short i = 0; i <= last_code_symbol; ++i) {
+            result << (int)code[i];
+        }
+
+        return result.str();
+    }
+
+    T symbol;
+    std::bitset<256> code;
+    short last_code_symbol;
+    Node* first_child;
+    Node* second_child;
+};
+
+typedef Node<char> CharNode;
+typedef Node<wchar_t> WCharNode;
+
+std::vector<std::pair<char, float>> getAlphabetSymbolFrequences(
+    std::string input
+) {
+    std::map<char, size_t> symbolCounts;
+
+    for (const auto& character : input) {
+        ++symbolCounts[character];
+    }
+
+    std::vector<std::pair<char, float>> alphabetSymbolFrequences;
+    size_t input_length = input.length();
+    for (const auto& symbolFrequencyPair : symbolCounts) {
+        alphabetSymbolFrequences.push_back(
+            std::make_pair(
+                symbolFrequencyPair.first,
+                (double)symbolFrequencyPair.second / input_length
+            )
+        );
+    }
+
+    return alphabetSymbolFrequences;
+}
+
+std::multimap<float, Node<char>*> getInitialTree(
+    std::vector<std::pair<char, float>> alphabet_data
+) {
+    std::multimap<float, Node<char>*> initialTree;
+
+    for (auto& current_symbol_data : alphabet_data) {
+        Node<char>* currentNode = new Node<char>(
+            current_symbol_data.first
+        );
+        initialTree.insert(std::make_pair(
+            current_symbol_data.second,
+            currentNode
+        ));
+    }
+
+    return initialTree;
+}
+
+std::vector<Node<char>*> getSymbolNodes(
+    std::multimap<float, Node<char>*>& initislTree
+) {
+    std::vector<Node<char>*> result;
+
+    for (const auto& current_pair : initislTree) {
+        result.push_back(current_pair.second);
+    }
+
+    return result;
+}
+
+Node<char>* buildTree(
+    std::multimap<float, Node<char>*>& tree
+) {
+    while (tree.size() > 1) {
+        Node<char>* last_node = tree.begin()->second;
+        float last_node_frequency = tree.begin()->first;
+        tree.erase(tree.begin());
+        Node<char>* penult_node = tree.begin()->second;
+        float penult_node_frequency = tree.begin()->first;
+        tree.erase(tree.begin());
+
+        Node<char>* newNode = new Node<char>(penult_node, last_node);
+        tree.insert(std::make_pair(
+            last_node_frequency + penult_node_frequency,
+            newNode
+        ));
+    }
+
+    return tree.begin()->second;
+}
+
+void initializeTreeLeafesCodes(
+    Node<char>* tree_node,
+    Node<char>* parent,
+    bool value
+) {
+    for (short i = 0; i <= parent->last_code_symbol; ++i) {
+        tree_node->code[i] = parent->code[i];
+    }
+    tree_node->code[parent->last_code_symbol + 1] = value;
+    tree_node->last_code_symbol = parent->last_code_symbol + 1;
+
+    if (tree_node->first_child) {
+        initializeTreeLeafesCodes(tree_node->first_child, tree_node, false);
+    }
+    if (tree_node->second_child) {
+        initializeTreeLeafesCodes(tree_node->second_child, tree_node, true);
+    }
+}
+
+void initializeTreeNodesCodes(Node<char>* root) {
+
+    initializeTreeLeafesCodes(
+        root->first_child, root, false
+    );
+    initializeTreeLeafesCodes(
+        root->second_child, root, true
+    );
+}
+
+std::string encodeString(
+    std::string input, 
+    std::vector<Node<char>*> symbolNodes
+) {
+    std::string encodedString = "";
+
+    for (const auto& symbol : input) {
+        for (const auto& node : symbolNodes) {
+            if (symbol == node->symbol) {
+                encodedString += node->getCodeStringRepresentation();
+                break;
+            }
+        }
+    }
+
+    return encodedString;
+}
+
+std::string decodeString(
+    std::string encoded_string,
+    std::vector<Node<char>*> symbolNodes
+) {
+    std::string decoded_string = "";
+
+    std::string currentCode = "";
+    for (const auto& symbol : encoded_string) {
+        currentCode += symbol;
+
+        for (const auto& node : symbolNodes) {
+            if (currentCode == node->getCodeStringRepresentation()) {
+                decoded_string += node->symbol;
+                currentCode = "";
+                break;
+            }
+        }
+    }
+
+    return decoded_string;
+}
+
+std::vector<std::vector<std::string>> completion_codes = {
+    {"00110101", "00110101"},
+    {"00111", "010"},
+    {"0111", "11"},
+    {"1000", "10"},
+    {"1011", "011"},
+    {"1100", "0011"},
+    {"1110", "0010"},
+    {"1111", "00011"},
+    {"10011", "000101"},
+    {"10100", "000100"},
+    {"00111", "0000100"},
+    {"01000", "0000101"},
+    {"001000", "0000111"},
+    {"000011", "00000100"},
+    {"110100", "00000111"},
+    {"110101", "000011000"},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"", ""},
+    {"00101101", "000001010100"},
+};
+std::map<unsigned short, std::vector<std::string>> additional_codes = {
+    {512, {"01100101", "0000001101100"}}
+};
+size_t completion_codes_max = 63;
+size_t additional_codes_max = 2560;
+
+std::vector<size_t> getRowSeries(const char* row, size_t columns) {
+    std::vector<size_t> result;
+    size_t current_series = 0;
+    char current_color = '0';
+
+    for (size_t i = 0; i < columns; ++i) {
+        if (row[i] != current_color) {
+            result.push_back(current_series);
+            current_series = 0;
+            current_color = current_color == '0' ? '1' : '0';
+        }
+        ++current_series;
+    }
+    result.push_back(current_series);
+
+    return result;
+}
+
+std::vector<std::string> encode_by_ccitt_group_3(
+    const char** input_image, size_t rows, size_t columns
+) {
+    std::vector<std::string> encoded_data;
+    std::vector<size_t> row_series;
+    const char* current_row;
+    std::string row_code;
+    size_t series_length;
+    size_t code_overflow = completion_codes_max + additional_codes_max;
+    size_t max_compound_code;
+
+    for (size_t i = 0; i < rows; ++i) {
+        current_row = input_image[i];
+        row_series = getRowSeries(current_row, columns);
+        row_code = "";
+
+        for (size_t i = 0; i < row_series.size(); ++i) {
+            series_length = row_series[i];
+            while (series_length > code_overflow) {
+                series_length -= additional_codes_max;
+                row_code += additional_codes[additional_codes_max][i % 2];
+            }
+
+            if (series_length > completion_codes_max) {
+                max_compound_code = (series_length >> 6) * 64;
+                series_length -= max_compound_code;
+                row_code += additional_codes[max_compound_code][i % 2];
+            }
+            row_code += completion_codes[series_length][i % 2];
+        }
+
+        encoded_data.push_back(row_code);
+    }
+
+    return encoded_data;
+}
+
+void print(std::vector<Node<char>*> symbolNodes) {
+    for (const auto& k : symbolNodes) {
+        std::cout << k->symbol << ' ';
+        for (int i = 0; i <= k->last_code_symbol; ++i) {
+            std::cout << k->code[i];
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Ukrainian");
+
+    std::vector<std::pair<char, float>> alphabet_data = {
+        std::make_pair('a', 0.5),
+        std::make_pair('b', 0.24),
+        std::make_pair('c', 0.15),
+        std::make_pair('d', 0.11)
+    };
+
+    std::string input_message = "TEXT THAT IS COMPRESSED";
+    std::vector<std::pair<char, float>> a = 
+        getAlphabetSymbolFrequences(input_message);
+    std::multimap<float, Node<char>*> initial_tree = getInitialTree(
+        a
+    );
+    std::vector<Node<char>*> symbolNodes = getSymbolNodes(initial_tree);
+
+    Node<char>* b = buildTree(initial_tree);
+    initializeTreeNodesCodes(b);
+
+    std::cout << "Коди символів рядка: " << std::endl;
+    print(symbolNodes);
+
+    std::string encodedString = encodeString(input_message, symbolNodes);
+    std::cout << "Закодоване значення: " << encodedString << std::endl;
+    std::string decodedString = decodeString(encodedString, symbolNodes);
+    std::cout << "Декодоване значення: " << decodedString << std::endl << std::endl;
+
+    const char* input_image[] = {
+        "111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001111111111", 
+    };
+
+    std::vector<std::string> encoded_image = encode_by_ccitt_group_3(
+        input_image, 1, 525
+    );
+    std::cout << "Compressed image rows:" << std::endl;
+    for (const auto& row : encoded_image) {
+        std::cout << row << std::endl;
+    }
+}
